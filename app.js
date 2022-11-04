2e5c09eb49d42f85648b7f75eeabf3c18d73be79
David Milton <davidmilton@Davids-MacBook-Pro.local>
2022-11-03 19:33:23 -0700

    a little more progress

--- app.js
+++ app.js
@@ -51,3 +51,24 @@ function showSlides(n) {
 // EDITED CODE FROM W3SCHOOLS ENDS HERE //
 // EDITED CODE FROM W3SCHOOLS ENDS HERE //
 // EDITED CODE FROM W3SCHOOLS ENDS HERE //
+
+
+
+//Grabbing Elements
+const openBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal')
+const close = document.getElementById('close')
+
+//Functions
+const openModal = () => {
+  modal.style.display = 'block';
+}
+
+const closeModal = () => {
+  modal.style.display = 'none';
+}
+
+//Event Listeners
+openBtn.addEventListener('click', openModal)
+
+close.addEventListener('click', closeModal)
